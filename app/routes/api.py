98b78144385ab16dfb0e2f1828ebad0e9b1254e7
shanelevites <srlevit94@gmail.com>
2023-08-14 18:42:32 -0700

    created route for new comments

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys 
 
@@ -64,5 +64,29 @@ def login():
   session['loggedIn'] = True
 
   return jsonify(id = user.id)
+
+@bp.route('/comments', methods=['POST'])
+def comment():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new comment
+    newComment = Comment(
+      #from front end
+      comment_text = data['comment_text'],
+      post_id = data['post_id'],
+      #stored in session
+      user_id = session.get('user_id')
+    )
+    db.add(newComment)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Comment failed'), 500
+
+  return jsonify(id = newComment.id)
 
 # {'username': 'shanetestfinal', 'email': 'final@email.com', 'password': 'final123'}
